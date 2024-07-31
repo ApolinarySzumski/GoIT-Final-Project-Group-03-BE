f03e506c2ec77c3314aa8c6ea1e5ef81cd775194
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-07-31 19:31:18 +0200

    Add herokuTest endpoint

--- routes/others.js
+++ routes/others.js
@@ -6,6 +6,10 @@ import { subscribeNewsletter } from "../controller/others.js";
 
 const route = express.Router();
 
+route.get('/', (req,res) => {
+    res.send('Hello World');
+});
+
 // route.post("/subscribe", subscribeNewsletter);
 
 export default route;
