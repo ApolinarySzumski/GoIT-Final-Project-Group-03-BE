3cfd333870981ebad2f10718f33148822a36dff4
Marta Tabor <leeloothefifth@googlemail.com>
2024-08-03 16:16:04 +0100

    thumbnails for own recipes logic add

--- routes/ownRecipes.js
+++ routes/ownRecipes.js
@@ -6,12 +6,13 @@ import authorization from "../middlewares/jwtMiddleware.js";
 import addMyRecipe from "../controller/recipes/addMyRecipe.js";
 import deleteMyRecipe from "../controller/recipes/deleteMyRecipe.js";
 import getOwnRecipes from "../controller/recipes/getMyRecipes.js";
+import upload from "../middlewares/multerMiddleware.js";
 
 const route = express.Router();
 
 route.use(authorization);
 
-route.post("/add", addMyRecipe);
+route.post("/add", upload.single("thumb"), addMyRecipe);
 route.delete("/:id", deleteMyRecipe);
 route.get("/", getOwnRecipes);
 
