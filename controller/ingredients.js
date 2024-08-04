bf4d1b8aca029614a6484a9146fc28c125079248
ApolinarySzumski <apolinary@poczta.onet.pl>
2024-08-04 11:59:11 +0200

    tried to do getRecipiesByIngredient controller

--- controller/ingredients.js
+++ controller/ingredients.js
@@ -3,35 +3,53 @@ import Ingredient from "../service/schemas/ingredient.js";
 import Recipe from "../service/schemas/recipe.js";
 
 export const getRecipesByIngredient = async (req, res, next) => {
-  const { ingredient } = req.params;
-  console.log(ingredient);
   try {
-    const recipes = await Recipe.find({});
-    const ingredientName = await Ingredient.findOne({ ttl: ingredient });
-    console.log(ingredientName);
-    const ingredientNameId = ingredientName._id;
-    console.log(ingredientNameId);
-    // const recipesWithIngredient = recipes.filter(
-    //   (r) => r.ingredients[0].id === ingredientNameId,
-    // );
+    const { ingredient } = req.query;
 
-    // const recipesWithIngredient = recipes
-    //   .map((recipe) => recipe.ingredients)[0]
-    //   .filter((element) => element.id === ingredientNameId);
+    console.log(ingredient);
 
-    const afterMap = recipes.map((recipe) => recipe.ingredients)[0];
-
-    if (afterMap === []) {
-      return res.status(404).json({
-        message: `Not found any recipes with ${ingredient} in database`,
-      });
+    if (!ingredient) {
+      return res.status(400).json({ message: "Ingredient is required" });
     }
 
-    res.json(afterMap);
-  } catch (error) {
-    console.log(error);
-    next(error);
+    const recipes = await Recipe.find({
+      ingredients: { $regex: ingredient, $options: "i" },
+    });
+
+    console.log(recipes);
+
+    if (recipes.length === 0) {
+      return res.status(404).json({ message: "No recipes found" });
+    }
+
+    res.json(recipes);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
+  // const { ingredient } = req.params;
+  // console.log(ingredient);
+  // try {
+  //   const recipes = await Recipe.find({});
+  //   // console.log(recipes);
+  //   const ingredients = await Ingredient.find({});
+  //   // console.log(ingredients);
+  //   const ingredientObject = ingredients.find((i) => i.ttl === ingredient);
+  //   // console.log(ingredientObject);
+  //   const ingredientObjectId = ingredientObject._id;
+  //   // console.log(ingredientObjectId);
+  //   const ingredientsFromRecipes = recipes.flatMap(
+  //     (recipe) => recipe.ingredients,
+  //   );
+  //   console.log(ingredientsFromRecipes);
+  //   const arrayOfIngredientsIdToCompare = ingredientsFromRecipes.find(
+  //     (recipe) => recipe.id === ingredientObjectId,
+  //   );
+  //   console.log(arrayOfIngredientsIdToCompare);
+  //   res.json(arrayOfIngredientsIdToCompare);
+  // } catch (error) {
+  //   console.log(error);
+  //   next(error);
+  // }
 };
 export const getIngredients = async (_, res, next) => {
   try {
--- routes/ingredients.js
+++ routes/ingredients.js
@@ -11,6 +11,6 @@ import authorization from "../middlewares/jwtMiddleware.js";
 const route = express.Router();
 
 route.get("/list", authorization, getIngredients);
-route.get("/:ingredient", authorization, getRecipesByIngredient);
+route.get("/", authorization, getRecipesByIngredient);
 
 export default route;
