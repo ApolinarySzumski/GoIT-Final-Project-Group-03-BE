5b61d91bdc47f0f9e1564379bb61369df57365d5
ApolinarySzumski <apolinary@poczta.onet.pl>
2024-08-05 08:45:41 +0200

    deleted unnecessary code

--- controller/ingredients.js
+++ controller/ingredients.js
@@ -14,53 +14,6 @@ const getRecipesByIngr = async (ingredientTitle) => {
   const recipes = await Recipe.find({ "ingredients.id": ingredient._id });
   return recipes;
 };
-//   try {
-//     const { ingredient } = req.query;
-
-//     console.log(ingredient);
-
-//     if (!ingredient) {
-//       return res.status(400).json({ message: "Ingredient is required" });
-//     }
-
-//     const recipes = await Recipe.find({
-//       ingredients: { $regex: ingredient, $options: "i" },
-//     });
-
-//     console.log(recipes);
-
-//     if (recipes.length === 0) {
-//       return res.status(404).json({ message: "No recipes found" });
-//     }
-
-//     res.json(recipes);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-//   const { ingredient } = req.params;
-//   console.log(ingredient);
-//   try {
-//     const recipes = await Recipe.find({});
-//     // console.log(recipes);
-//     const ingredients = await Ingredient.find({});
-//     // console.log(ingredients);
-//     const ingredientObject = ingredients.find((i) => i.ttl === ingredient);
-//     // console.log(ingredientObject);
-//     const ingredientObjectId = ingredientObject._id;
-//     // console.log(ingredientObjectId);
-//     const ingredientsFromRecipes = recipes.flatMap(
-//       (recipe) => recipe.ingredients,
-//     );
-//     console.log(ingredientsFromRecipes);
-//     const arrayOfIngredientsIdToCompare = ingredientsFromRecipes.find(
-//       (recipe) => recipe.id === ingredientObjectId,
-//     );
-//     console.log(arrayOfIngredientsIdToCompare);
-//     res.json(arrayOfIngredientsIdToCompare);
-//   } catch (error) {
-//     console.log(error);
-//     next(error);
-//   }
 
 export const getRecipesByIngredient = async (req, res, next) => {
   const { ingredient } = req.params;
