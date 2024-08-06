c74d27f75eaff5364d98ff4c47bf257f7150f064
ApolinarySzumski <apolinary@poczta.onet.pl>
2024-08-06 19:10:42 +0200

    tried to do first endpoint documentation

--- routes/auth.js
+++ routes/auth.js
@@ -21,6 +21,33 @@ const route = express.Router();
 
 route.use(passport.initialize());
 
+/**
+ * @openapi
+ * '/users/register':
+ *  post:
+ *   tags:
+ *   - User
+ *   summary: Register a user
+ *  requestBody:
+ *    required: true
+ *    content:
+ *      application/json:
+ *        schema:
+ *          $ref: '#/components/schemas/CreateUserInput'
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/CreateUserResponse'
+ *      409:
+ *        description: Conflict
+ *      400:
+ *        description: Bad request
+ *
+ */
+
 route.post("/register", validateBody(registerSchema), registerUser);
 route.post("/login", validateBody(loginSchema), loginUser);
 route.get("/current", authorization, getUser);
--- service/schemas/user.js
+++ service/schemas/user.js
@@ -2,6 +2,41 @@
 import { Schema, model } from "mongoose";
 import bCrypt from "bcrypt";
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    CreateUserInput:
+ *      type: object
+ *      required:
+ *        - name
+ *        - email
+ *        - password
+ *      properties:
+ *        name:
+ *          type: string
+ *          default: exampleName
+ *        email:
+ *          type: string
+ *          default: example@email.com
+ *        password:
+ *          type: string
+ *          default: examplePassword123
+ *    CreateUserResponse:
+ *      type: object
+ *      properties:
+ *        _id:
+ *          type: string
+ *        name:
+ *          type: string
+ *        email:
+ *          type: string
+ *        token:
+ *          type: string
+ *        subscribe:
+ *          type: boolean
+ */
+
 const userSchema = new Schema({
   name: {
     type: String,
