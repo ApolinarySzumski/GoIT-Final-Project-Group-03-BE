887d2d281112dc4d312848391fef122e14ecfd61
ApolinarySzumski <apolinary@poczta.onet.pl>
2024-08-07 17:06:25 +0200

    stash changes

--- routes/others.js
+++ routes/others.js
@@ -2,16 +2,14 @@
 import express from "express";
 
 // my modules
-import addToFavorites from '../controller/favorites/addToFavorites.js'
-import getFavoriteRecipes from '../controller/favorites/getFavoriteRecipes.js'
-import addIngredientToList from '../controller/shopping-list/addIngredientToList.js'
-import removeIngredientFromList from '../controller/shopping-list/removeIngredientFromList.js'
-import getIngredientList from '../controller/shopping-list/getIngredientList.js'
 import getMyFavoriteRecipes from "../controller/recipes/getMyFavoriteRecipes.js";
 import searchRecipeByKeyword from "../controller/recipes/searchRecipeByKeyword.js";
 import setMyFavoriteRecipe from "../controller/recipes/setMyFavoriteRecipe.js";
 import subscribeNewsletter from "../controller/recipes/subscribeNewsletter.js";
 import unsetMyFavoriteRecipe from "../controller/recipes/unsetMyFavoriteRecipe.js";
+import addIngredientToList from "../controller/shopping-list/addIngredientToList.js";
+import getIngredientList from "../controller/shopping-list/getIngredientList.js";
+import removeIngredientFromList from "../controller/shopping-list/removeIngredientFromList.js";
 import authorization from "../middlewares/jwtMiddleware.js";
 
 const route = express.Router();
@@ -35,6 +33,6 @@ route.post("/shopping-list", addIngredientToList);
 
 route.delete("/shopping-list", removeIngredientFromList);
 
-route.get('/shopping-list', getIngredientList)
+route.get("/shopping-list", getIngredientList);
 
 export default route;
