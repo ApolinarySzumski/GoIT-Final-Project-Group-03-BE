a752760a561002566a4eb37541cb6d19be63c8b3
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-08-08 17:33:12 +0200

    Update getIngredientNames.js
    Add Id to information about Ingredient

--- controller/recipes/getIngredientNames.js
+++ controller/recipes/getIngredientNames.js
@@ -3,13 +3,17 @@ import Ingredient from "../../service/schemas/ingredient.js";
 const getIngredients = async (_, res, next) => {
   try {
     const ingredients = await Ingredient.find({});
-    const ingredientsList = ingredients.map((i) => i.ttl);
 
-    if (!ingredients) {
+    if (ingredients.length === 0) {
       return res
         .status(404)
         .json({ message: "Not found any ingredients in database" });
     }
+
+    const ingredientsList = ingredients.map((i) => ({
+      _id: i._id,
+      ttl: i.ttl
+    }));
 
     res.json(ingredientsList);
   } catch (error) {
