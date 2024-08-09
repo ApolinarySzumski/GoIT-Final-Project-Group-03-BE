bd6952b8f1b571c986454a9140d103cda0cedf1c
ApolinarySzumski <apolinary@poczta.onet.pl>
2024-08-09 10:24:30 +0200

    try to fix 2

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const tmpDir = path.join(__dirname, "../public/tmp/");
+const tmpDir = path.join(__dirname, "../public/tmp");
 
 if (!fs.existsSync(tmpDir)) {
   fs.mkdirSync(tmpDir);
