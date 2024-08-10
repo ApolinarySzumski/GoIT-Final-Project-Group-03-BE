d8d7d9aec4a385ee56299330a6539bbcfb100920
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-08-10 11:21:59 +0200

    Update getMyFavoriteRecipes.js
    Add Pagination

--- controller/recipes/getMyFavoriteRecipes.js
+++ controller/recipes/getMyFavoriteRecipes.js
@@ -2,15 +2,29 @@ import User from "../../service/schemas/user.js";
 
 const getMyFavoriteRecipes = async (req, res, next) => {
   const userId = req.user._id;
+  const { page = 1, limit = 4} = req.query
 
   try {
-    const user = await User.findById(userId).populate("favorites");
+    const user = await User.findById(userId).populate({
+      path: "favorites",
+      options: {
+        skip: (page - 1) * limit,
+        limit: parseInt(limit),
+      },
+    });
 
-    if (user.favorites.length === 0) {
+    const totalRecipes = user.favorites.length;
+    const totalPages = Math.ceil(totalRecipes/limit);
+
+    if (totalRecipes === 0) {
       return res.status(404).json({ message: "No favorite recipes found" });
     }
 
-    res.status(200).json(user.favorites);
+    res.status(200).json({
+      favorites: user.favorites,
+      totalPages,
+      currentPage: parseInt(page)
+    });
   } catch (err) {
     next(err);
   }
