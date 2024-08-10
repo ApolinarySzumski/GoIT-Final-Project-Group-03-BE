786a7a2c7dc76e591b82aa2b9fe800af3354ab4f
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-08-10 13:56:49 +0200

    Change Joi for registerSchema

--- controller/users/registerUser.js
+++ controller/users/registerUser.js
@@ -1,33 +1,8 @@
 //my modules
 import User from "../../service/schemas/user.js";
-import Joi from "joi";
-
-const userValidationSchema = Joi.object({
-  name: Joi.string().required().messages({
-    "string.empty": "Name is required",
-  }),
-  email: Joi.string().email().required().messages({
-    "string.empty": "Email is required",
-    "string.email": "Invalid email format",
-  }),
-  password: Joi.string()
-    .min(8)
-    .pattern(new RegExp("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>]).*$"))
-    .required()
-    .messages({
-      "string.empty": "Password is required",
-      "string.min": "Password must be at least 8 characters long",
-      "string.pattern.base": "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-    }),
-});
 
 const registerUser = async (req, res, next) => {
   const { name, email, password } = req.body;
-  const { error } = userValidationSchema.validate({ name, email, password });
-
-  if (error) {
-    return res.status(400).json({ message: error.details[0].message });
-  }
 
   try {
     const existingUser = await User.findOne({ email });
