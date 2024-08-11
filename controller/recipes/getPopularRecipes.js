ac5dea32d46a7563de61b70b12c0f4aefb32ab82
Marta Tabor <leeloothefifth@googlemail.com>
2024-08-11 17:48:35 +0100

    update to popular recipe preview instead of image and limit set to 4

--- controller/recipes/getPopularRecipes.js
+++ controller/recipes/getPopularRecipes.js
@@ -1,42 +1,42 @@
-import Recipe from '../../service/schemas/recipe.js';
-import User from '../../service/schemas/user.js';
+import Recipe from "../../service/schemas/recipe.js";
+import User from "../../service/schemas/user.js";
 
 const getPopularRecipes = async (req, res, next) => {
-    try {
-        const popularRecipes = await Recipe.aggregate([
-            {
-                $lookup: {
-                    from: User.collection.name,
-                    localField: '_id',
-                    foreignField: 'favorites',
-                    as: 'usersWhoLike'
-                }
-            },
-            {
-                $project: {
-                    _id: 1,
-                    title: 1,
-                    description: 1,
-                    image: 1,
-                    favoritesCount: { $size: '$usersWhoLike'}
-                }
-            },
-            {
-                $sort: { favoritesCount: -1}
-            },
-            {
-                $limit: 10
-            }
-        ]);
+  try {
+    const popularRecipes = await Recipe.aggregate([
+      {
+        $lookup: {
+          from: User.collection.name,
+          localField: "_id",
+          foreignField: "favorites",
+          as: "usersWhoLike",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          description: 1,
+          preview: 1,
+          favoritesCount: { $size: "$usersWhoLike" },
+        },
+      },
+      {
+        $sort: { favoritesCount: -1 },
+      },
+      {
+        $limit: 4,
+      },
+    ]);
 
-        if(popularRecipes.length === 0) {
-            return res.status(404).json({message: 'No popular recipes found'});
-        }
-
-        res.status(200).json(popularRecipes)
-    } catch (err) {
-        next(err)
+    if (popularRecipes.length === 0) {
+      return res.status(404).json({ message: "No popular recipes found" });
     }
+
+    res.status(200).json(popularRecipes);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default getPopularRecipes;
